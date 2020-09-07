04350e44ed93fbd55441f002927fa7a158b7972b
jmoney515 <70900219+jmoney515@users.noreply.github.com>
2020-09-07 08:37:06 -0500

    Initial upload

---
+++ ISPBusiness.java
@@ -0,0 +1,56 @@
+package edu.iastate.cs228.hw1;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author <<Write your name here>>
+ *
+ * The ISPBusiness class performs simulation over a grid 
+ * plain with cells occupied by different TownCell types.
+ *
+ */
+public class ISPBusiness {
+	
+	/**
+	 * Returns a new Town object with updated grid value for next billing cycle.
+	 * @param tOld: old/current Town object.
+	 * @return: New town object.
+	 */
+	public static Town updatePlain(Town tOld) {
+		Town tNew = new Town(tOld.getLength(), tOld.getWidth());
+		//TODO: Write your code here.
+		return tNew;
+	}
+	
+	/**
+	 * Returns the profit for the current state in the town grid.
+	 * @param town
+	 * @return
+	 */
+	public static int getProfit(Town town) {
+		//TODO: Write/update your code here.
+		return 0;
+	}
+	
+
+	/**
+	 * Main method. Interact with the user and ask if user wants to specify elements of grid
+	 *  via an input file (option: 1) or wants to generate it randomly (option: 2).
+	 *  
+	 *  Depending on the user choice, create the Town object using respective constructor and
+	 *  if user choice is to populate it randomly, then populate the grid here.
+	 *  
+	 *  Finally: For 12 billing cycle calculate the profit and update town object (for each cycle).
+	 *  Print the final profit in terms of %. You should only print the integer part (Just print the 
+	 *  integer value. Example if profit is 35.56%, your output should be just: 35).
+	 *  
+	 * Note that this method does not throws any exception, thus you need to handle all the exceptions
+	 * in it.
+	 * 
+	 * @param args
+	 * 
+	 */
+	public static void main(String []args) {
+		//TODO: Write your code here.
+	}
+}
